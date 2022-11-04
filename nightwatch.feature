3b1c9e5012a573a437634c0f860589daec719642
ahmadjalal1122 <84639723+ahmadjalal1122@users.noreply.github.com>
2022-11-04 15:31:39 +0500

    Add files via upload

--- nightwatch.feature
+++ nightwatch.feature
@@ -1,13 +1,19 @@
-Feature: Review
-Background: Background name
-  Given I open the website on a website browser
+Feature: Magento Change Shipping Address
+Background:
+  Given I logged in as a valid user
 
-@a @b
-Scenario: Add Review
-  When I click on the relevant product
-  And I click on the Review tab
-  And add the Rating
-  And type Nickname
-  And type Summary
-  And add Review
-  And click on submit review  button
+Scenario Outline: change shipping address of customer
+  Given I open edit shipping address page
+  When I enter <first name>
+  And I enter1 <last name>
+  And I enter2 <phone no>
+  And I enter3 <street address>
+  And I enter4 <Zip code>
+  And I enter5 <city>
+  And I enter6 <country>
+  And I clicks on save address button
+  Then my shipping address has been saved
+
+Examples:
+|first name |last name |phone no      |street address         |Zip code   |city        |country   |
+|"Ahmad"    |"alii"     |"03477704011" |"404 A block Sabzazar" |"554433"   |"islamabad" |"Pakistan"|
--- nightwatch.js
+++ nightwatch.js
@@ -36,110 +36,81 @@
 const {Given,When ,Then } = require('@cucumber/cucumber');
 const { WebElement } = require('selenium-webdriver');
 
-// Given(/^I logged in as a valid user$/, function() {
-//  return  browser
-//                 .url('http://magento2demo.firebearstudio.com/')
-//                 .click("body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.authorization-link > a")
+Given(/^I logged in as a valid user$/, function() {
+ return  browser
+                .url('https://magento.jsmartfix.com/')
+                .click("body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.authorization-link > a")
             
-//                 .verify.title('Customer Login')
+                .verify.title('Customer Login')
          
-//                 .waitForElementVisible('body',3000)
-//                 .clearValue('#email')
-//                 .clearValue('#pass')
-//                 .setValue('#email','madnijalal1880561@gmail.com')
-//                 .setValue('#pass','_ahmad1122')
-//                 .click('#send2')
-//                 .pause(4*1000)
+                .waitForElementVisible('body',3000)
+                .clearValue('#email')
+                .clearValue('#pass')
+                .setValue('#email','madnij@gmail.com')
+                .setValue('#pass','123abc__')
+                .click('#send2')
+                .pause(4*1000)
                 
-// });
-
-// Given('I open edit shipping address page', function() {
-//    return browser
-//                 .click('body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.customer-welcome > span > button')
-//                 .click('body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.customer-welcome.active > div > ul > li:nth-child(1)')
-//                 .click('#maincontent > div.columns > div.column.main > div.block.block-dashboard-addresses > div.block-content > div.box.box-shipping-address > div.box-actions > a')
-//                 .pause(1*1000)
-// });
-
-// When('I enter first name,last name,phone no,street address,Zip code,city,country', function() {
-//   return browser
-//                .verify.title('Edit Address')
-//                .clearValue('#firstname')
-//                .setValue('#firstname','Amjad')
-//                .clearValue('#lastname')
-//                .setValue('#lastname','Islam')
-//                .clearValue('#company')
-//                .setValue('#company','NESPak')
-//                .clearValue('#telephone')
-//                .setValue('#telephone','03477704011')
-//                .clearValue('#street_1')
-//                .setValue('#street_1','404 Multan Road Sabzazar')
-//                .setValue('#country','Pakistan')
-//                .clearValue('#region')
-//                .setValue('#region','Sindh')
-//                .clearValue('#city')
-//                .setValue('#city','Karachi')
-//                .clearValue('#zip')
-//                .setValue('#zip','123456')
-               
-//                .pause(1*1000)
-// });
-// Given('I clicks on save address button', function() {
-//   return browser
-//                 .click('#form-validate > div > div.primary > button')    
-//                 .pause(1*1000)
-// });
-// Then('my shipping address has been saved', function(title) {
-
-//   return browser
-//                 .assert.titleEquals(title)
-//                 .pause(3*1000)
-// });
-
-
-
-Given('I open the website on a website browser', function(){
-  return browser
-               .url('http://magento2demo.firebearstudio.com/men.html')
-               .click('#maincontent > div.columns > div.sidebar.sidebar-main > div.widget.block.block-static-block > div > ul:nth-child(2) > li:nth-child(2) > a')
-               
 });
-When('I click on the relevant product', function(){
+
+Given('I open edit shipping address page', function() {
    return browser
-                .click('#product-item-info_340 > a > span')
-              
+                .click('body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.customer-welcome > span > button')
+                .click('body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.customer-welcome.active > div > ul > li:nth-child(1)')
+                .click('#maincontent > div.columns > div.column.main > div.block.block-dashboard-addresses > div.block-content > div.box.box-shipping-address > div.box-actions > a')
+                .pause(1*1000)
 });
-Given('I click on the Review tab', function(){
-  return browser
-               .click('#tab-label-reviews')
-               
-});
-Given('add the Rating', function(){
-  // browser.elementIdElements r=findElement(By.id("#Rating_1"));
-  return browser
-               .click('input[type=radio]')
-});
-Given('type Nickname', function(){
-  return browser
-                 .clearValue('#nickname_field')
-                 .setValue('#nickname_field','niki')
-              
-});
-Given('type Summary', function(){
-  return browser
-              .clearValue('#summary_field')
-              .setValue('#summary_field','warm jacket')
-              
+When('I enter {string}',async function(string){
+  // console.log(searchTerm);
+   browser
+  .verify.title('Add New Address')
+  .clearValue('#firstname')
+  .setValue('#firstname',[string,browser.Keys.ENTER])
+   .pause(1*1000)
 });
 
-Given('add Review', function(){
-  return browser
-              .clearValue('#review_field')
-              .setValue('#review_field','very noice jacket')
-              
+Given('I enter1 {string}',async function (string){
+  console.log("when");
+   browser
+  .clearValue('#lastname')
+  .setValue('#lastname',[string,browser.Keys.ENTER])
+  .pause(1*1000)
+  
 });
-Given('click on submit review  button', function(){
+Given('I enter2 {string}', function(string) {
+   browser 
+   .clearValue('#telephone')
+  .setValue('#telephone',[string,browser.Keys.ENTER])
+});
+Given('I enter3 {string}', function(string) {
+   browser 
+          .clearValue('#street_1')
+          .setValue('#street_1',[string,browser.Keys.ENTER])
+});
+Given('I enter4 {string}', function(string) {
+   browser 
+           .clearValue('#zip')
+           .setValue('#zip',[string,browser.Keys.ENTER])
+});
+
+Given('I enter5 {string}', function(string) {
+   browser 
+          .clearValue('#city')
+          .setValue('#city',[string,browser.Keys.ENTER])
+});
+Given('I enter6 {string}', function(string) {
+   browser 
+           .clearValue('#country')
+           .setValue('#country',[string,browser.Keys.ENTER])
+});
+Given('I clicks on save address button', function() {
+   browser
+                .click('#form-validate > div > div.primary > button')    
+                .pause(3*1000)
+});
+Then('my shipping address has been saved', function(title) {
+
   return browser
-               .click('#review-form > div > div > button')
-              .pause(5*1000)
+                .assert.titleEquals(title)
+                .pause(3*1000)
 });
