ba83015e96c18c7c3d25685ef3fbd1adf211c5fe
ahmadjalal1122 <84639723+ahmadjalal1122@users.noreply.github.com>
2022-11-05 19:56:44 +0500

    Create test-countriesCheck.py

---
+++ 20L-1305/API Test/test-countriesCheck.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import jsonpath
+
+url = "http://magento.jsmartfix.com/rest/all/V1/directory/countries"
+token = "s8ohb34lwfdv2743tex3kl4ot8wj88x8"
+
+headers = {
+   "Accept": "application/json",
+   "Content-Type": "application/json",
+   "Authorization" : 'Bearer '+ token
+}
+
+def test_CountryList():
+    response = requests.get(url,headers=headers)
+    response_json = json.loads(response.text)
+    for index in range(len(response_json)):
+        id = response_json[index]['id']
+        location = response_json[index]['full_name_locale']
+        print('Country Id = ', id, '   Country Location = ', location)
+    assert response.status_code == 200
+
+
+if __name__ == "__main__":
+    test_CountryList()
